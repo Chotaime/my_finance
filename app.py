d1fd8535063946967bcdac01cf77a5786f7a2cda
Agnik Chakraborty <agnik.chakraborty.ece26@heritageit.edu.in>
2025-06-22 03:53:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import joblib
 import pandas as pd
 
@@ -7,6 +8,8 @@ model = joblib.load("model.pkl")
 
 # Create the Flask app
 app = Flask(__name__)
+CORS(app)  # Enables CORS for all domains by default
+
 
 @app.route('/')
 def home():
